1566d9674a92215ce60826f5b3977e50f4986548
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-07 14:04:03 +0200

    one improvement and then to review

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://hidden-mesa-03483.herokuapp.com/v1/task';
+  var apiRoot = 'https://hidden-mesa-03483.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
